eaf91b22dfb5dae20bf3971d73dd7f4e4d3bbca2
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-26 10:51:05 +0530

    files added

--- echo.sh
+++ echo.sh
@@ -2,6 +2,11 @@
 
 echo 'this is our first shell               script'
 
+: '
+This is multiline comments.
+It spans across multiple lines.
+'
+
 echo -e "\033[0;32m success message here"
 echo -e "\033[0;31m fail message here"
 echo -e "\033[0;33m warning message here"
