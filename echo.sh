6596905e0d6dc3b67150d8a356126b61490c2f76
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-26 10:31:05 +0530

    files added

---
+++ echo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'this is our first shell               script'
+
+echo -e "\033[0;32m success message here"
---
+++ first.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello World"
+echo "this is our first shellscript"
+
+ls
+cal 
+
+pwd
+sleep 10
