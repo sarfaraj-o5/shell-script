b08e9905671cb5f1d37231303dde9bf3626ad99a
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-26 10:52:07 +0530

    files added

--- echo.sh
+++ echo.sh
@@ -10,3 +10,8 @@ It spans across multiple lines.
 echo -e "\033[0;32m success message here"
 echo -e "\033[0;31m fail message here"
 echo -e "\033[0;33m warning message here"
+
+: <<'END_COMMENT'
+This is multiline comments.
+It spans across multiple lines.
+END_COMMENT
