71283349797a493017c0bf97e0e87571bee2e2ef
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-26 11:34:03 +0530

    files added

---
+++ system_variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# system vars usage
+
+echo "User: $USER"
+echo "Home directory: $HOME"
+echo "Shell: $SHELL"
+echo "Machine name: $HOSTNAME"
+echo "Working directory: $PWD"
+echo "User ID: $UID"
+echo "System Path: $PATH"
+echo "Operating system: $OSTYPE"
+echo "Parent ProcessID: $PPID"
+echo "Script runtime: $SECONDS seconds"
+echo "Random number: $RANDOM"
+echo "Terminal dimension: $LINES lines x $COLUMNS columns"
+echo "Temporary directory: $TMPDIR"
