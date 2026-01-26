9feb0cf4ba8e8a95bc831dbc4a83d823f5fe0a99
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-26 11:20:24 +0530

    files added

---
+++ variable_scope.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# variables with different cases
+name="Amar"
+NAME="Adam"
+nAmE="Bakar"
+
+echo "Different Variables: ${name}, ${NAME}, ${nAmE}"
+
+# using underscores in vars names
+_variableName="First variable"
+echo "${_variableName}"
+
+# invalid vars names
+
+3variable="Invalid"
+variable-name="Invalid"
---
+++ variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## variables examples
+name="Amar"
+age="20"
+work="programm"
+var="ing"
+
+# priint values
+echo "Name: ${name}"
+echo "Age: ${age}"
+echo "I am ${work}${var}."
+
+# incorrect vars ref
+echo "I am $working" # working not defined
+
