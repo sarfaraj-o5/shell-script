82f62a526b454a1584d897bd45484444b8b9d692
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-26 11:00:38 +0530

    files added

--- echo.sh
+++ echo.sh
@@ -15,3 +15,19 @@ echo -e "\033[0;33m warning message here"
 This is multiline comments.
 It spans across multiple lines.
 END_COMMENT
+
+### Escape sequences ###
+echo "This is\na new line"
+echo -e "This\tis\ta\ttabbed\toutput"
+echo -e "Vertical\vtab\vexample"
+echo -e "Multi-line with escape:\nLine 1\nLine2\nLine 3"
+
+# strong quotes examples
+echo 'This is single-quoted, so \n will not be interpreted'
+
+## combining comments and echo
+echo "
+######################Script Starting ##########
+Purpose: Install NGNIX
+####################################
+"
