08a63ff998cffe599738d45352cded8a0bc6070c
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-27 16:04:55 +0530

    files added

---
+++ set-default-value.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# read -p "please enter your name" name
+
+# name=${name:-World}
+
+# echo "Hello ${name^}"
+
+# yourname=${unsetvariable-Man}
+# echo $yourname
+
+# myname=""
+# mytestname=${myname:-kali}
+# echo ${mytestname}
+
+### var is set or not
+# name="guru"
+: ${1:?" please set vars values. "}
+echo "i am here"
