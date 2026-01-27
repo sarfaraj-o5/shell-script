f1834a383e95eddece608bb7bc89da113310cb43
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-27 11:12:23 +0530

    files added

---
+++ convert-string.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ## first chars to upper case
+# string="my name is Amar"
+# echo "${string^}" 
+
+# ## upper case entire line
+# string="my name is amr"
+# echo "${string^^}"
+
+# ## first chars to lower case
+# string="my name is Amar"
+# echo "${string,}" 
+
+# ## lower case entire line
+# string="my name is amr"
+# echo "${string,,}"
+
+## length of string
+string="My name is amar"
+echo "Length of string is ${#string}"
