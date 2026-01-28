d12bdab4a43beddcfafc4ac1ab6e410c85d808fd
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-28 10:21:06 +0530

    files added

---
+++ functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### FUNCTIONS ###
+function install(){
+    ### installation code.
+    echo "installationcode1"
+}
+
+configuration(){
+    ## config code
+    echo "configcode1"
+}
+
+function deploy {
+    ## deploy code.
+    echo "deploy code 1."
+}
+
+configuration
+install
+deploy
