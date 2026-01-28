c6c69af6d86dae25137c624e36e4ffd0e5c2f1ce
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-01-28 10:36:03 +0530

    files added

--- functions.sh
+++ functions.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
-### FUNCTIONS ###
+# ### FUNCTIONS ###
+# function install(){
+#     ### installation code.
+#     echo "installationcode1"
+# }
+
+# configuration(){
+#     ## config code
+#     echo "configcode1"
+# }
+
+# function deploy {
+#     ## deploy code.
+#     echo "deploy code 1."
+# }
+
+# configuration
+# install
+# deploy
+
+
+### PARAMETERS TO A FUNCTIONS ###
 function install(){
-    ### installation code.
-    echo "installationcode1"
+    echo "executing ${FUNCNAME} - start"
+    echo "installing ${1}"
+    echo "executing ${FUNCNAME} -end"
 }
 
-configuration(){
-    ## config code
-    echo "configcode1"
+function configuration(){
+    echo "config ${1}"
+    echo "${FUNCNAME}"
 }
 
 function deploy {
-    ## deploy code.
-    echo "deploy code 1."
+    echo "config ${1}"
+    echo "${FUNCNAME}"
 }
 
-configuration
-install
-deploy
+configuration "nginx"
+install "nginx"
+deploy "webapplication"
